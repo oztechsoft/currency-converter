108a3d14ec8a484b7428bef516371fe2c0c931ea
Ali Naqi <alinaqishahidi@gmail.com>
2023-08-23 23:13:28 +1000

    Implemented methods for converting currency and adding to csv file

--- src/CurrencyConverter.php
+++ src/CurrencyConverter.php
@@ -4,9 +4,30 @@ namespace App;
 
 class CurrencyConverter
 {
-    public function convert(float $amount, Currency $fromCurrency, Currency $toCurrency): float
+    private $amount;
+    private $fromCurrency;
+    private $convertedAmount;
+    private $toCurrency;
+    
+    public function __construct(float $amount, Currency $fromCurrency, Currency $toCurrency)
     {
-        return $amount * ($toCurrency->getConversionRate() / $fromCurrency->getConversionRate());
+        $this->amount = $amount;
+        $this->fromCurrency = $fromCurrency;
+        $this->toCurrency = $toCurrency;
+    }
+
+    public function convert(): float
+    {
+        $this->convertedAmount = $this->amount * ($this->toCurrency->getConversionRate() / $this->fromCurrency->getConversionRate());
+        return $this->convertedAmount;
+    }
+
+    public function convertToCSV(){
+        $fromCurrencyCode = $this->fromCurrency->getCode();
+        $toCurrencyCode = $this->toCurrency->getCode();
+    
+        $csvData = "{$this->amount} {$fromCurrencyCode}, {$this->convertedAmount} {$toCurrencyCode}\n";
+        file_put_contents('currency_conversion.csv', $csvData, FILE_APPEND);
     }
 }
 
